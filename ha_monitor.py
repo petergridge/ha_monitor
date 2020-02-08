f44c3c1952bee2c6701b573866f964899ac09abc
petergridge <40281772+petergridge@users.noreply.github.com>
2020-02-08 18:12:25 +1100

    Improved behaviour when restarting home assistant
    Fix bug - restart of HA caused python script to fail
    Update refresh all behavior to use better parameters
    Update refresh all to behave better on HA restart

--- ha_monitor.py
+++ ha_monitor.py
@@ -28,7 +28,7 @@ import psutil
 DEBUG = 0 #set to 1 to get debug output
 
 WAIT_TIME = 60 #[s] Time to wait between each refresh
-
+REFRESH_ALL = 600 #[s] time to send all states even if unchanged
 HAServer = "http://192.168.1.62:8123" #Home assistant server
 #ha_monitor long lived token
 AUTHKEY = "YOUR.AUTHKEY.eyJpc3MiOiI4OGY1MDdkNjY0MmE0NTBjYTU1MWFlNzlhZWEyZDY3MiIsImlhdCI6MTU4MTA0OTIwMSwiZXhwIjoxODk2NDA5MjAxfQ.oo1SLDPXH2Nx5pnAkzeGEbdgkZpzqBoHuRU9E3i4ex0"
@@ -54,40 +54,59 @@ def HAPost(sensorname,state,unitmeasure,friendlyname,icon):
         response = requests.post(url, headers=headers, json=data)
         if DEBUG == 1:
             print(url, response, state)
-    except(Timeout):
-        #HA is not available, rebooting maybe, just keep retrying
+        if str(response) == '<Response [201]>':
+            #Home Assistant is creating this for the first time so ensure all the states are written
+            #states are written if this is a restart of HA
+            return 1
+    except(requests.exceptions.Timeout):
+        #HA is slow, just keep retrying
+        if DEBUG == 1:
+            print('requests.exceptions.Timeout')
+        pass #do nothing
+    except(requests.exceptions.ConnectionError):
+        #This is the exception I get when HA is rebooting, just keep retrying
+        #and force a refresh of all states
+        if DEBUG == 1:
+            print('requests.exceptions.ConnectionError')
+        return 1
         pass #do nothing
 
 try:
     while (1):
-        #Only POST a value if the temp has changed or every 10 cycles
+        #Only POST a value if the state has changed or every REFRESH_ALL/WAIT_TIME cycles
         #to minimise the network taffic
-        #on restart of Home Assistant it may take 10 cycles to get
-        #all the sensors back
 
         # Read CPU temperature
         cpuTempFile=open("/sys/class/thermal/thermal_zone0/temp","r")
-        cpuTemp=float(cpuTempFile.read())/1000
+        cpuTemp=float(format(float(cpuTempFile.read())/1000,'.2f'))
         cpuTempFile.close()
-
         if cpuTemp != lastcpuTemp:
-            HAPost("temperature",cpuTemp,"C","CPU Temp","mdi:thermometer")
+            ret = HAPost("temperature",cpuTemp,"°C","CPU Temp","mdi:thermometer")
             lastcpuTemp = cpuTemp
+
+        # Read CPU processor use
         cpu_percent = psutil.cpu_percent()
         if cpu_percent != lastcpupercent:
             HAPost("cpu_percent",cpu_percent,"%","Processor Use","mdi:memory")
             lastcpupercent = cpu_percent
+
+        #Read Memory use
         virtual_memory = psutil.virtual_memory()[2]
         if virtual_memory != lastvirtualmamory:
-            HAPost("memory_used",virtual_memory,"%","Memory Use","mdi:memory")
+            ret = HAPost("memory_used",virtual_memory,"%","Memory Use","mdi:memory")
             lastvirtualmamory = virtual_memory
+
+        #Read Disk use
         disk_usage = psutil.disk_usage('/')[3]
         if disk_usage != lastdiskusage:
-            HAPost("disk_used",disk_usage,"%","Disk Use","mdi:harddisk")
+            ret = HAPost("disk_used",disk_usage,"%","Disk Use","mdi:harddisk")
             lastdiskusage = disk_usage
 
         count = count + 1
-        if count >= 10:
+        if count >= REFRESH_ALL/WAIT_TIME or ret == 1:
+            if DEBUG == 1:
+                print('Resend All Sensors')
+            #force next cycle to send all states
             count = 0
             lastcpuTemp = 0
             lastcpupercent = 0
